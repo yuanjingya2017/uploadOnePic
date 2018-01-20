a044d9bc088fdc42b1973e50ff42e3b2ac0ec998
mdnjs1 <18166891304@163.com>
2018-01-20 09:07:53 +0800

    Create test.vue

---
+++ test.vue
@@ -0,0 +1,34 @@
+<template>
+  <uploadPic @getSonData="getSData"></uploadPic>
+</template>
+
+<script>
+  import uploadPic from './uploadPic'
+  export default {
+  data(){
+    return {
+      //保存图片base64的数组
+      pics:[]
+    }
+  },
+  mounted(){
+
+  },
+  components:{
+    uploadPic
+  },
+  computed:{
+
+  },
+  methods:{
+    getSData:function (value) {
+      var _this = this;
+      _this.pics = value;
+      console.log(value)
+    }
+  }
+};
+</script>
+<style scoped>
+
+</style>
